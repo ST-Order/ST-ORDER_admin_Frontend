1609ddc9eaa74ce9d9e2df6391dafef1972a2320
pp.navillera <pp.navillera@gmail.com>
2025-03-01 23:36:14 +0900

    Style: add Logout button

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Header({}) {
   return (
-    <div className="w-full border-b border-gray3 px-14 py-6">
+    <div className="flex justify-between w-full py-6 border-b border-gray3 px-14">
       <Image
         aria-hidden
         src="/graphics/st-order-vertical.svg"
@@ -9,6 +9,9 @@ export default function Header({}) {
         width={308}
         height={61}
       />
+      <button className="py-5 text-xl leading-tight border px-14 border-blue2 text-blue2 rounded-xl">
+        로그아웃
+      </button>
     </div>
   );
 }
