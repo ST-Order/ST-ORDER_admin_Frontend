83190759d620af08f50dd5c9ed615983441078be
pp.navillera <pp.navillera@gmail.com>
2025-03-03 03:06:33 +0900

    Feat: Wrap header in a link to navigate to the index page

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Header({}) {
   return (
-    <div className="flex justify-between w-full py-6 border-b border-gray3 px-14">
+    <Link
+      href="/index"
+      className="flex justify-between w-full py-6 border-b border-gray3 px-14"
+    >
       <Image
         aria-hidden
         src="/graphics/st-order-vertical.svg"
@@ -12,6 +16,7 @@ export default function Header({}) {
       <button className="py-5 text-xl leading-tight border px-14 border-blue2 text-blue2 rounded-xl">
         로그아웃
       </button>
-    </div>
+    </Link>
+    // todo: 로그인 되어있을때만 헤더의 홈 버튼 활성화
   );
 }
