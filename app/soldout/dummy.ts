d044adf3b12b9361735ac26500b9393c2c60eb6c
pp.navillera <pp.navillera@gmail.com>
2025-03-05 01:47:45 +0900

    Feat: Add dummy data for sold-out menu items

---
+++ app/soldout/dummy.ts
@@ -0,0 +1,18 @@
+export const DUMMY_DATA = {
+  menus: [
+    {
+      menuId: 1,
+      menuName: "삼겹덮밥",
+      menuImage: "https://example.com/menu1.png",
+      price: 5000,
+      isSoldOut: true,
+    },
+    {
+      menuId: 2,
+      menuName: "제육덮밥",
+      menuImage: "https://example.com/menu2.png",
+      price: 5000,
+      isSoldOut: true,
+    },
+  ],
+};
