7d3df86b920394081333ac6837d786bc51b49040
pp.navillera <pp.navillera@gmail.com>
2025-03-12 10:28:21 +0900

    Feat: Add notice registration form with various fields and image preview
    - Add form fields for title, visibility period, category, optional image, and content
    - Implement image preview functionality for the optional image field
    - Integrate useForm hook for form handling and validation
    - Add category selection with dropdown and validation

--- app/notice/register/page.tsx
+++ app/notice/register/page.tsx
@@ -1,61 +1,21 @@
 "use client";
 
+import { RegisterNotice } from "@/types/types";
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 export default function Page() {
   const [category, setCategory] = useState<boolean>(false);
   const [selectedCat, setSelectedCat] = useState<string>();
   const [imagePreview, setImagePreview] = useState<string | null>(null);
 
-  const OptionItem = ({
-    name,
-    index,
-    length,
-  }: {
-    name: string;
-    index: number;
-    length: number;
-  }) => {
-    let borderClass = "";
-    if (index === length - 1) {
-      // 마지막 항목: hover 시에만 위쪽 테두리
-      borderClass = "hover:border-t";
-    } else if (index === 0) {
-      // 첫 번째 항목: 항상 아래쪽 테두리
-      borderClass = "border-b";
-    } else {
-      // 중간 항목: 항상 아래쪽 테두리, hover 시 위쪽 테두리 추가
-      borderClass = "border-b hover:border-t";
-    }
-
-    return (
-      <div
-        onClick={() => {
-          setSelectedCat(name);
-          setCategory(false);
-        }}
-        className={`cursor-pointer w-full py-3 px-6 ${borderClass} hover:border-[#adb3c0] border-[#dee1e6] bg-[#f9f9fa] hover:bg-[#dee1e6] text-xl font-normal font-['Inter'] leading-tight`}
-      >
-        {name}
-      </div>
-    );
-  };
-
-  const OptionList = ({ categories }: { categories: string[] }) => {
-    return (
-      <div className="w-full rounded-xl border border-[#adb3c0] flex-col bg-white overflow-hidden">
-        {categories.map((category, index) => (
-          <OptionItem
-            key={index}
-            name={category}
-            index={index}
-            length={categories.length}
-          />
-        ))}
-      </div>
-    );
-  };
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<RegisterNotice>();
 
   const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -68,15 +28,21 @@ export default function Page() {
     }
   };
 
+  const onSubmit: SubmitHandler<RegisterNotice> = (data) => {
+    console.log(data);
+  };
   return (
-    <div className="flex flex-col w-full gap-5 px-10 overflow-y-scroll lg:px-20 py-7">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col w-full gap-5 px-10 overflow-y-scroll lg:px-20 py-7"
+    >
       <div className="flex flex-col w-full gap-7">
         {/* title */}
         <div className="text-black text-4xl font-bold font-['Inter'] leading-9 ">
           공지사항 등록
         </div>
         {/* form */}
-        <form className="flex flex-col items-start justify-start gap-7">
+        <div className="flex flex-col items-start justify-start gap-7">
           {/* 1st row */}
           <div className="flex items-start w-full gap-10 lg:gap-40 lg:w-2/3">
             <div className="flex flex-col items-start justify-start flex-1 gap-4">
@@ -84,6 +50,7 @@ export default function Page() {
                 제목
               </label>
               <input
+                {...register("title", { required: true })}
                 placeholder="공지사항의 제목을 입력해주세요."
                 className="px-4 py-3 bg-[#ecedf0] rounded-xl border border-gray3 text-xl font-normal font-['Inter'] leading-tight w-full"
               />
@@ -95,10 +62,10 @@ export default function Page() {
               <div className="flex items-center gap-6 wrap max-w-96">
                 <div className="flex items-center gap-2">
                   <input
+                    {...register("visibilityPeriod", { required: true })}
                     type="radio"
                     className="w-5 h-5"
-                    value={1}
-                    name="duration"
+                    value="day"
                   />
                   <div className="text-black text-xl font-normal font-['Inter'] leading-tight">
                     하루(24시간)
@@ -106,10 +73,10 @@ export default function Page() {
                 </div>
                 <div className="flex items-center gap-2">
                   <input
+                    {...register("visibilityPeriod", { required: true })}
                     type="radio"
                     className="w-5 h-5"
-                    value={2}
-                    name="duration"
+                    value="week"
                   />
                   <div className="text-black text-xl font-normal font-['Inter'] leading-tight">
                     일주일(7일)
@@ -117,10 +84,10 @@ export default function Page() {
                 </div>
                 <div className="flex items-center gap-2">
                   <input
+                    {...register("visibilityPeriod", { required: true })}
                     type="radio"
                     className="w-5 h-5"
-                    value={3}
-                    name="duration"
+                    value="month"
                   />
                   <div className="text-black text-xl font-normal font-['Inter'] leading-tight">
                     한 달(30일)
@@ -134,35 +101,66 @@ export default function Page() {
             <div className="text-black text-2xl font-semibold font-['Inter'] leading-normal">
               카테고리
             </div>
-            <div className="relative w-96">
-              <div
-                onClick={() => {
-                  setCategory(!category);
-                }}
-                className="flex items-center justify-between w-full px-4 py-3 border cursor-pointer bg-gray1 rounded-xl border-gray4"
-              >
-                <div
-                  className={`${
-                    !selectedCat ? "text-gray5" : "text-black"
-                  }  text-xl font-normal font-['Inter'] leading-tight`}
-                >
-                  {!selectedCat
-                    ? "공지사항의 유형을 선택해주세요."
-                    : selectedCat}
-                </div>
-                <Image
-                  src="/icons/dropdown_down.svg"
-                  width={14}
-                  height={14}
-                  alt=""
-                />
-              </div>
-              {category && (
-                <div className="absolute left-0 z-10 w-full mt-1 top-full">
-                  <OptionList categories={["신메뉴 출시", "품절 안내"]} />
+            <Controller
+              name="category"
+              control={control}
+              rules={{ required: "카테고리를 선택해주세요" }}
+              render={({ field }) => (
+                <div className="relative w-96">
+                  <div
+                    onClick={() => {
+                      setCategory(!category);
+                    }}
+                    className="flex items-center justify-between w-full px-4 py-3 border cursor-pointer bg-gray1 rounded-xl border-gray4"
+                  >
+                    <div
+                      className={`${
+                        !field.value ? "text-gray5" : "text-black"
+                      }  text-xl font-normal font-['Inter'] leading-tight`}
+                    >
+                      {!field.value
+                        ? "공지사항의 유형을 선택해주세요."
+                        : field.value}
+                    </div>
+                    <Image
+                      src="/icons/dropdown_down.svg"
+                      width={14}
+                      height={14}
+                      alt=""
+                    />
+                  </div>
+                  {category && (
+                    <div className="absolute left-0 z-10 w-full mt-1 top-full">
+                      <div className="w-full rounded-xl border border-[#adb3c0] flex-col bg-white overflow-hidden">
+                        {["신메뉴 출시", "품절 안내"].map((cat, index, arr) => {
+                          let borderClass = "";
+                          if (index === arr.length - 1) {
+                            borderClass = "hover:border-t";
+                          } else if (index === 0) {
+                            borderClass = "border-b";
+                          } else {
+                            borderClass = "border-b hover:border-t";
+                          }
+
+                          return (
+                            <div
+                              key={index}
+                              onClick={() => {
+                                field.onChange(cat);
+                                setCategory(false);
+                              }}
+                              className={`cursor-pointer w-full py-3 px-6 ${borderClass} hover:border-[#adb3c0] border-[#dee1e6] bg-[#f9f9fa] hover:bg-[#dee1e6] text-xl font-normal font-['Inter'] leading-tight`}
+                            >
+                              {cat}
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  )}
                 </div>
               )}
-            </div>
+            />
           </div>
           {/* 사진 */}
           <div className="flex flex-col items-start justify-center h-40 gap-4">
@@ -215,21 +213,30 @@ export default function Page() {
               </div>
             </div>
             <textarea
+              {...register("content", { required: true })}
               maxLength={275}
               placeholder="공지사항의 내용을 입력해 주세요."
               className="px-6 py-5 h-44 bg-[#ecedf0] rounded-xl border border-gray3 text-xl font-normal font-['Inter'] leading-tight w-full"
             />
           </div>
-        </form>
+        </div>
       </div>
+      {errors.category && (
+        <div className="text-base font-normal leading-none text-red2">
+          카테고리 선택은 필수입니다.
+        </div>
+      )}
       <div className="flex items-center justify-center gap-4">
         <button className="w-[268px] py-4 bg-red1 rounded-2xl text-[22px] font-medium leading-snug text-white text-center">
           취소
         </button>
-        <button className="w-[268px] py-4 bg-blue2 rounded-2xl text-[22px] font-medium leading-snug text-white text-center">
+        <button
+          type="submit"
+          className="w-[268px] py-4 bg-blue2 rounded-2xl text-[22px] font-medium leading-snug text-white text-center"
+        >
           등록완료
         </button>
       </div>
-    </div>
+    </form>
   );
 }
