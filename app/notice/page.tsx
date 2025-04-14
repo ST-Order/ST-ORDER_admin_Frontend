24182f832eb5ec944504b740ca79010bfcd43a8d
pp.navillera <pp.navillera@gmail.com>
2025-04-14 20:32:35 +0900

    Refactor: Replace button with Link component for '공지 등록' and update notice item links for improved navigation

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
+  const noticeItem = {
+    id: 1,
+  };
   return (
     <div className="flex flex-col items-center w-full h-screen gap-4 px-20 bg-gray2 py-7">
       {/* title */}
@@ -9,15 +12,15 @@ export default function Page() {
         <div className="text-black text-4xl font-bold font-['Inter'] leading-9">
           공지사항
         </div>
-        <button className="px-6 py-3 bg-[#0e34c9] rounded-xl justify-center items-center gap-3 flex">
+        <Link
+          href="/notice/register"
+          className="px-6 py-3 bg-[#0e34c9] rounded-xl justify-center items-center gap-3 flex"
+        >
           <Image src="/icons/plus.svg" alt="plus" width={20} height={20} />
-          <Link
-            href="/notice/register"
-            className="text-white text-[22px] font-medium font-['Inter'] leading-snug"
-          >
+          <div className="text-white text-[22px] font-medium font-['Inter'] leading-snug">
             공지 등록
-          </Link>
-        </button>
+          </div>
+        </Link>
       </div>
       {/* rows section */}
       <div className="flex flex-col flex-1 w-full gap-4 pt-4 pb-24 overflow-y-auto">
@@ -27,7 +30,10 @@ export default function Page() {
             <div className="h-[88px] px-9 py-6 text-[40px] font-bold font-['Inter'] leading-10">
               1
             </div>
-            <div className="flex flex-col items-start gap-6">
+            <Link
+              href={`/notice/${noticeItem.id}`}
+              className="flex flex-col items-start gap-6 min-w-96"
+            >
               <div className="h-8 px-4 py-1.5 bg-[#fcd7e3] rounded-lg text-noticeOutOfStockText text-xl font-medium font-['Inter'] leading-tight ">
                 품절
               </div>
@@ -37,12 +43,15 @@ export default function Page() {
               <div className="text-gray5 text-xl font-normal font-['Inter'] leading-tight">
                 2025.03.20 17:09
               </div>
-            </div>
+            </Link>
           </div>
           <div className="flex items-center gap-3">
-            <button className=" p-7 bg-blue2 rounded-2xl text-white text-2xl font-medium font-['Inter'] leading-7">
+            <Link
+              href={`/notice/${noticeItem.id}/edit`}
+              className=" p-7 bg-blue2 rounded-2xl text-white text-2xl font-medium font-['Inter'] leading-7"
+            >
               수정
-            </button>
+            </Link>
             <button className=" p-7 bg-red1 rounded-2xl text-white text-2xl font-medium font-['Inter'] leading-7">
               삭제
             </button>
